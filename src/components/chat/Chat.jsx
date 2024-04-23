69774c60450bdc04a31df46fd51bbf79731bada9
Khojiakbar <usmondev@outlook.kr>
2024-04-23 21:15:41 +0900

    message DSP Style

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,41 @@
+import { useState } from 'react';
 import './chat.css';
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
-	return <div className='chat'>Chat</div>;
+	const [open, setOpen] = useState(false);
+	return (
+		<div className='chat'>
+			<div className='top'>
+				<div className='user'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov KH</span>
+						<p>Lorem ipsum dolor sit sdf.</p>
+					</div>
+				</div>
+				<div className='icons'>
+					<img src='./phone.png' alt='phone' />
+					<img src='./video.png' alt='video' />
+					<img src='./info.png' alt='info' />
+				</div>
+			</div>
+			<div className='center'></div>
+			<div className='bottom'>
+				<div className='icons'>
+					<img src='./img.png' alt='' />
+					<img src='./camera.png' alt='' />
+					<img src='./mic.png' alt='' />
+				</div>
+				<input type='text' placeholder='Type a message...' />
+				<div className='emoji'>
+					<img src='./emoji.png' alt='emoji' onClick={() => setOpen(prev => !prev)} />
+					<EmojiPicker open={open} onEmojiClick={handleEmoji} />
+				</div>
+				<button className='sendButton'>Send</button>
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './chatList.css';
 
 const ChatList = () => {
@@ -12,6 +13,71 @@ const ChatList = () => {
 				</div>
 				<img src={addMode ? './minus.png' : './plus.png'} alt='plus' className='add' onClick={() => setAddMode(prev => !prev)} />
 			</div>
+			<Scrollbars style={{ width: '100%', height: '85%' }}>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+				<div className='item'>
+					<img src='./avatar.png' alt='avatar' />
+					<div className='texts'>
+						<span>Usmonov Kh</span>
+						<p>Hello</p>
+					</div>
+				</div>
+			</Scrollbars>
 		</div>
 	);
 };
